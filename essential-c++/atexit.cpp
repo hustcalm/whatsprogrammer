325b49739ab04585c6343132c4e47bab796d7c21
hustcalm <licalmer@gmail.com>
2014-06-04 15:05:25 +0800

    called when the program terminates normally

---
+++ essential-c++/atexit.cpp
@@ -0,0 +1,23 @@
+#include <stdio.h>      /* puts */
+#include <stdlib.h>     /* atexit */
+
+void fnExit1 (void)
+{
+  puts ("Exit function 1.");
+}
+
+void fnExit2 (void)
+{
+  puts ("Exit function 2.");
+}
+
+int main ()
+{
+  atexit (fnExit1);
+  atexit (fnExit2);
+  puts ("Main function.");
+  return 0;
+}
+
+
+
